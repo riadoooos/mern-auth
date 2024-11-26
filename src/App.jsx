dd675f670ab33a55bd863cc6749b2a171087f158
riadoooos <lotfi.said1955@gmail.com>
2024-11-26 17:07:06 +0100

    Add Header commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
 import { Profile } from "./pages/Profile";
 import { About } from "./pages/About";
+import { Header } from "./components/Header";
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/sign-in" element={<SignIn />} />
