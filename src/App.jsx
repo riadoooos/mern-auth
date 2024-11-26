0c4a69f2d297054d34dbf6729da382cae50ec788
riadoooos <lotfi.said1955@gmail.com>
2024-11-26 15:55:32 +0100

    Add Routes commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { Profile } from "./pages/Profile";
+import { About } from "./pages/About";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/sign-in" element={<SignIn />} />
+        <Route exact path="/sign-up" element={<SignUp />} />
+        <Route exact path="/profile" element={<Profile />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
