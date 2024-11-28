a6c22e73442a6e9360dfa0f2713a29843bbe4c9d
riadoooos <lotfi.said1955@gmail.com>
2024-11-28 12:30:53 +0100

    sign-up commit

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,43 @@
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const SignIn = () => {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Sign-In
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont Have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-500">Sign up</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
